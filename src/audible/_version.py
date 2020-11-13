19a4ada09f1f7445e27b5105c1f08e9c9b58c98f
kennedn <kennedn@msn.com>
2020-11-13 20:34:03 +0000

    Changed package name to avoid conflicts

--- src/audible/_version.py
+++ src/audible/_version.py
@@ -1,9 +1,9 @@
-__title__ = "audible"
+__title__ = "audible-kennedn"
 __description__ = "A(Sync) Interface for internal Audible API written in " \
                   "pure Python."
-__url__ = "https://github.com/mkb79/audible"
+__url__ = "https://github.com/kennedn/audible"
 __version__ = "0.5.dev0"
-__author__ = "mkb79"
-__author_email__ = "mkb79@hackitall.de"
+__author__ = "mkb79, kennedn"
+__author_email__ = "mkb79@hackitall.de, kennedn@msn.com"
 __license__ = "AGPL"
 __status__ = "Development"
